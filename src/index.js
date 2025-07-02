4fe19e88da035aeabd4db62e0502c9bd3ed98d56
Mxguelito <montejovictor779@gmail.com>
2025-07-01 23:25:15 -0300

    ⚙ seccion Login en Proceso...

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export{default as App} from'./App';
+export * from'./context/AuthContext';
+export * from'./main';
+export * from'./style/breackpoints';
+export * from'./style/themes';
+export * from'./style/variables';
+export * from'./supabase/supabase.config';
+export * from'./utils/dataEstatica';
