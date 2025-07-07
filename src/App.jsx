51fdf15a908acd67fe10187b49b1f45801ec2527
Mxguelito <montejovictor779@gmail.com>
2025-07-07 02:19:18 -0300

    ⚙Corrigiendo errores

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import{AuthContextProvider}from "./index"
+import{AuthContextProvider,MyRoutes}from "./index"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <p style = {{color:"red"}}>Visible afuera del contexto</p>
+   
       <AuthContextProvider>
-        <span>Hola desde context</span>
+       <MyRoutes/>
       </AuthContextProvider>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+//App
 export{default as App} from'./App';
+//Context
 export * from'./context/AuthContext';
 export * from'./main';
 export * from'./style/breackpoints';
 export * from'./style/themes';
 export * from'./style/variables';
+//Supabase
 export * from'./supabase/supabase.config';
+//Utils
 export * from'./utils/dataEstatica';
+//Pages
+export {Home} from'./pages/Home'
+export{MyRoutes}from'./routers/routes'
+//Templates
+export{HomeTemplate} from './components/templates/HomeTemplate';
